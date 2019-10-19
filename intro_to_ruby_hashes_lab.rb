0e267de47e0c4eabb5e886f509081bce5b239c69
Bretton B Holmes <famunkwriter@me.com>
2019-10-19 12:43:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,25 +1,53 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+	monopoly = {
+		:railroads =>{}
+	}
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
+	monopoly = {
+		:railroads => {
+			:pieces => 4
+		}
+	}# When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
+	monopoly = {
+		:railroads => {
+			:pieces => 4,
+			:rent_in_dollars => {
+				:one_piece_owned =>{},
+				:two_pieces_owned =>{},
+				:three_pieces_owned =>{},
+				:four_pieces_owned =>{}
+			},
+			:names =>{
+				
+			}
+		}
+	}# When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
+	monopoly = {
+		:railroads => {
+			:pieces => 4,
+			:rent_in_dollars =>
+			{
+				:one_piece_owned =>{},
+				:two_pieces_owned =>{},
+				:three_pieces_owned =>{},
+				:four_pieces_owned =>{}
+			}
+		}
+	}# When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
