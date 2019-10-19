859eea7f650caa01a8aab0306c10ec9de0f8405b
Bretton B Holmes <famunkwriter@me.com>
2019-10-19 12:50:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ def monopoly_with_third_tier
 			:names =>{
 				:reading_railroad =>{},
         :pennsylvania_railroad =>{},
-        :SunsetLtd =>{},
+        :b_and_o_railroad =>{},
         :Southwestern =>{}
 			}
 		}
